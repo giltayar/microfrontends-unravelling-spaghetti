ed5611315f0afce92e5287dc22a426345ae0e0b0
Gil Tayar <gil@tayar.org>
2021-12-04 20:55:06 +0200

    ms: remove healthz controller module
    [bilt-with-bilt]

--- microfrontends-site/src/microfrontends-site.js
+++ microfrontends-site/src/microfrontends-site.js
@@ -1,23 +1,22 @@
 import fastify from 'fastify'
-import {healthz} from './controllers/healthz-controller.js'
 import proxy from 'fastify-http-proxy'
 
 /** @param {{}} options */
 export async function makeWebApp({}) {
   const app = fastify()
 
-  app.get('/healthz', healthz)
+  app.get('/healthz', async () => ({}))
   app.get('/', async (_req, reply) => reply.redirect('/todo/'))
 
   app.register(proxy, {
-    upstream: 'http://about-page-server/',
     prefix: '/about/',
     rewritePrefix: '/',
+    upstream: 'http://about-page-server/',
   })
   app.register(proxy, {
-    upstream: 'http://todo-list-page-server/',
     prefix: '/todo/',
     rewritePrefix: '/',
+    upstream: 'http://todo-list-page-server/',
   })
 
   return {app}
